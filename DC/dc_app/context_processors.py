a3ea9cd36db6c083103bf31e256a7d3c0a628339
teslead123 <teslead123@gmail.com>
2025-09-09 15:21:36 +0530

    Delete DC directory

--- DC/dc_app/context_processors.py
+++
@@ -1,9 +0,0 @@
-def user_session_data(request):
-    """
-    Makes role and employee_id available in all templates.
-    """
-    return {
-        'session_role': request.session.get('role'),
-        'session_employee_id': request.session.get('employee_id'),
-        'username': request.session.get('username'),
-    }
